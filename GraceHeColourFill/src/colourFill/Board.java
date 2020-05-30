c8399dbe6e7cf1dce1ebccd9bd21a14904876b3d
che5488 <cindy.he5@student.tdsb.on.ca>
2020-05-30 02:41:45 -0400

    i'll update the to do stuff in trello?

--- GraceHeColourFill/src/colourFill/Board.java
+++ GraceHeColourFill/src/colourFill/Board.java
@@ -3,55 +3,65 @@ package colourFill;
 import colourFill.Cell;
 
 public class Board {
-	
+
 	private Cell[][] board;
-	private int rows;
-	private int cols;
+	private int numRows;
+	private int numCols;
 
 	/**
 	 * Constructor for Boards.
+	 * 
 	 * @param aRows number of rows in board
 	 * @param aCols number of columns in board
 	 */
-	public Board(int aRows, int aCols) {
-		board = new Cell[aRows][aCols];
-		rows = aRows;
-		cols = aCols;
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
+	public Board(int numRows, int numCols) {
+		board = new Cell[numRows][numCols];
+		this.numRows = numRows;
+		this.numCols = numCols;
+		for (int i = 0; i < numRows; i++) {
+			for (int j = 0; j < numCols; j++) {
 				board[i][j] = new Cell(); // no color
 			}
 		}
 	}
-	
+
 	public char getCellState(int row, int col) {
 		return board[row][col].getState();
 	}
 
-	public void floodFill(int col, int row,char targetColour, char replaceColour) {
-		if (targetColour == replaceColour||board[row][col].getState() != targetColour) {
-			return; 
-		}
-		else{board[row][col].setState(replaceColour);}
+	public int getNumRows() {
+		return numRows;
+	}
 
-		if (col>0) {
-			floodFill((col-1),row,targetColour, replaceColour);
+	public int getNumCols() {
+		return numCols;
+	}
+
+	public void floodFill(int row, int col, char targetColour, char replaceColour) {
+		if (targetColour == replaceColour || board[row][col].getState() != targetColour) {
+			return;
+		} else {
+			board[row][col].setState(replaceColour);
 		}
-		if(col<6) {
-			floodFill((col+1),row,targetColour, replaceColour);
+
+		if (col > 0) {
+			floodFill(row, (col - 1), targetColour, replaceColour);
 		}
-		if(row>0) {
-			floodFill(col,(row-1),targetColour, replaceColour);
+		if (col < numCols-1) {
+			floodFill( row, (col + 1), targetColour, replaceColour);
 		}
-		if(row<5) {
-			floodFill(col,(row+1),targetColour, replaceColour);
+		if (row > 0) {
+			floodFill((row - 1),col, targetColour, replaceColour);
+		}
+		if (row < numRows-1) {
+			floodFill((row + 1),col, targetColour, replaceColour);
 		}
 	}
-	
+
 	public void display() {
 		System.out.println("BOARD");
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
+		for (int i = 0; i < numRows; i++) {
+			for (int j = 0; j < numCols; j++) {
 				System.out.printf("%s ", board[i][j]);
 			}
 			System.out.println();
--- GraceHeColourFill/src/colourFill/Driver.java
+++ GraceHeColourFill/src/colourFill/Driver.java
@@ -16,36 +16,129 @@ public class Driver {
 	public static void main(String[] args) {
 
 		// Setup constants for the Board
-		final int ROWS = 6;
-		final int COLS = 7;
-
-		// create the board
-		Board board = new Board(ROWS, COLS);
-		board.display();
+		final int NUMROWS = 7;
+		final int NUMCOLS = 7;
 
 		// console input
-		boolean done = false;
-		String value = "";
-		char colour = 'f';
+		Scanner in = new Scanner(System.in);
+		char colour = ' ';
 
-		int count = 0;
-		
-		while (done != true) {
-			done = true;
-			colour = getColour();
-			board.floodFill(0, 0, board.getCellState(0, 0), colour);
+		boolean inPlay = true;
+
+		while (inPlay) {
+			boolean single = false;
+			boolean singleWin = false;
+
+			boolean multi = false;
+			boolean player1Win = false;
+			boolean player2Win = false;
+
+			boolean robot = false;
+			boolean robotWin = false;
+			boolean humanWin = false;
+
+			String mode = "";
+
+			int count = 0;
+
+			mode = getMode();// for now, 1 is single, 2 is multi,3 is robot, create buttons later
+			if (mode.equals("1")) {
+				single = true;
+			} else if (mode.equals("2")) {
+				multi = true;
+			} else if (mode.equals("3")) {
+				robot = true;
+			}
+
+			// create the board
+			Board board = new Board(NUMROWS, NUMCOLS);
 			board.display();
-			count++;
-			for(int row = 0; row<6;row++) {
-				for (int col = 0;col<7;col++) {
-					if (board.getCellState(0,0)!= board.getCellState(row,col)) {
-						done = false;
+
+			if (single) {
+				while (!singleWin) {
+					singleWin = true;
+					colour = getColour();
+					board.floodFill(0, 0, board.getCellState(0, 0), colour);
+					board.display();
+					count++;
+					singleWin = isWinner(board);
+				}
+			} else if (multi) {
+				while (!player1Win && !player2Win) {
+					player1Win = true;
+					player2Win = true;
+					System.out.print("Player 1, ");
+					colour = getColour();// more clear when there are buttons for whose turn it is
+					board.floodFill(0, 0, board.getCellState(0, 0), colour);
+					board.display();
+					player1Win = isWinner(board);
+
+					if (!player1Win) {
+						System.out.print("Player 2, ");
+						colour = getColour();
+						board.floodFill(NUMROWS - 1, NUMCOLS - 1, board.getCellState(NUMROWS - 1, NUMCOLS - 1), colour);
+						board.display();
+						player2Win = isWinner(board);
 					}
+					player2Win = false;
+				}
+			} else if (robot) {
+				while (!humanWin && !robotWin) {
+					humanWin = true;
+					robotWin = true;
+					colour = getColour();
+					board.floodFill(0, 0, board.getCellState(0, 0), colour);
+					board.display();
+					count++;
+					humanWin = isWinner(board);
+
+					if (!humanWin) {
+						System.out.println("Robot is going...");
+						colour = getRobotColour(board);
+						board.floodFill(NUMROWS - 1, NUMCOLS - 1, board.getCellState(NUMROWS - 1, NUMCOLS - 1), colour);
+						board.display();
+						robotWin = isWinner(board);
+					}
+					robotWin = false;
 				}
 			}
-		}
-		System.out.println("You used" + count + " clicks");
+			if (singleWin) {
+				System.out.println(count + " clicks used!");
+			} else if (player1Win) {
+				System.out.println("Winner: Player 1!");
+			} else if (player2Win) {
+				System.out.println("Winner: Player 2!");
+			} else if (robotWin) {
+				System.out.println("Winner: Robot!");
+			} else if (humanWin) {
+				System.out.println("Winner: You!");
+			}
 
+			System.out.println("Play again?");// buttons later, so just Y or N for now
+			String repeat = in.next();
+			if (repeat.equals("N")) {
+				inPlay = false;
+			}
+
+		}
+		System.out.println("Bye!");
+	}
+
+	private static String getMode() {
+		Scanner in = new Scanner(System.in);
+		boolean valid = false;
+		String mode = "";
+		while (valid != true) {
+			System.out.println("Single player, 2 player, or vs robot?");// for now, 1 is single, 2 is multi, 3 is robot,
+																		// create buttons later
+			mode = in.next();
+			if (mode.equals("1") || mode.equals("2") || mode.contentEquals("3")) {
+				valid = true;
+			} else {
+				valid = false;
+			}
+		}
+		return mode;
 	}
 
 	private static char getColour() {
@@ -53,7 +146,7 @@ public class Driver {
 		boolean valid = false;
 		String colour = "";
 
-		while (valid != true) {
+		while (!valid) {
 			String prompt = String.format("Which Colour (" + "R" + "B" + "Y" + "G" + "): ");
 			System.out.println(prompt);
 			colour = in.nextLine();
@@ -64,5 +157,53 @@ public class Driver {
 			}
 		}
 		return colour.charAt(0);
+	}
+
+	private static boolean isWinner(Board board) {
+		int numRows = board.getNumRows();
+		int numCols = board.getNumCols();
+		int row = 0;
+		int col = 0;
+
+		for (int r = 0; r < numRows; r++) {
+			for (int c = 0; c < numCols; c++) {
+				if (board.getCellState(row, col) != board.getCellState(r, c)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private static char getRobotColour(Board board) {
+		int[] numEachColour = new int[4];
+		char[] colours = { 'G', 'R', 'B', 'Y' };
+		int numRows = board.getNumRows();
+		int numCols = board.getNumCols();
+		char greatestColour = colours[0];
+
+		for (int r = 0; r < numRows; r++) {
+			for (int c = 0; c < numCols; c++) {
+				if (board.getCellState(r, c) == 'G') {
+					numEachColour[0]++;
+				} else if (board.getCellState(r, c) == 'R') {
+					numEachColour[1]++;
+				} else if (board.getCellState(r, c) == 'B') {
+					numEachColour[2]++;
+				} else if (board.getCellState(r, c) == 'Y') {
+					numEachColour[3]++;
+				}
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			if (numEachColour[i] <= numEachColour[i + 1]) {
+				greatestColour = colours[i + 1];
+			}
+		}
+
+		return greatestColour;
+
 	}
 }
